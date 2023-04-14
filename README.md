8857e4b7187a65560ad1dfff7548b9ef1c417341
IzaanArain <102476680+IzaanArain@users.noreply.github.com>
2023-04-14 19:01:04 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-task-bootcamp-palindome
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yzw5bw)
---
+++ index.js
@@ -0,0 +1,25 @@
+function pal(str) {
+  // console.log(str.split(''));
+  // const rev = str.split('').reverse().join('');
+  // console.log(rev);
+
+  var rev = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    // console.log(str[i]);
+    rev += str[i];
+  }
+  console.log(rev);
+  if (str === rev) {
+    console.log(`this word is a palindrome: "${str}" , palindrum: "${rev}"`);
+    alert(`this word is a palindrome: "${str}" , palindrum: "${rev}"`);
+  } else {
+    console.log(
+      `this is not palindrome: "${str}", after reversing word "${rev}"`
+    );
+    alert(`this is not palindrome: "${str}", after reversing word "${rev}"`);
+  }
+}
+
+// console.log('Enter a palindrome');
+// const userInput = prompt('Enter a word that is a palindrome');
+// pal(userInput);
